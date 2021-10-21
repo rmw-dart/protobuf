f85580e1da221d5355a94cdf2043a134c8342acc
gcxfd <i@rmw.link>
2021-10-21 15:32:21 +0800

    🔶

---
+++ dart/test.pbjson.dart
@@ -0,0 +1,22 @@
+///
+//  Generated code. Do not modify.
+//  source: test.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use searchRequestDescriptor instead')
+const SearchRequest$json = const {
+  '1': 'SearchRequest',
+  '2': const [
+    const {'1': 'query', '3': 1, '4': 1, '5': 9, '10': 'query'},
+    const {'1': 'page_number', '3': 2, '4': 1, '5': 5, '10': 'pageNumber'},
+    const {'1': 'result_per_page', '3': 3, '4': 1, '5': 5, '10': 'resultPerPage'},
+  ],
+};
+
+/// Descriptor for `SearchRequest`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List searchRequestDescriptor = $convert.base64Decode('Cg1TZWFyY2hSZXF1ZXN0EhQKBXF1ZXJ5GAEgASgJUgVxdWVyeRIfCgtwYWdlX251bWJlchgCIAEoBVIKcGFnZU51bWJlchImCg9yZXN1bHRfcGVyX3BhZ2UYAyABKAVSDXJlc3VsdFBlclBhZ2U=');
---
+++ make.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+DIR=$( dirname $(realpath "$0") )
+cd $DIR
+
+protoc --dart_out=dart main.proto
